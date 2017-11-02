755c840afa169a65695ddabd2583cbad5e64446d
tkdalsdjaak <30948682+tkdalsdjaak@users.noreply.github.com>
2017-11-02 22:24:29 +0900

    1

--- src/hs/sdh/comment/BlockComment.java
+++ src/hs/sdh/comment/BlockComment.java
@@ -12,7 +12,7 @@ class SystemOutPrintln1
 	public static void main(String[] args)
 	{
 		System.out.println(7);      /* 정수의 출력 */
-		System.out.println(3.15);
+		System.out.println(3.15);   /* 실수의 출력 */
 		System.out.println("3+5=" + 8);	
 		System.out.println(3.15 + "는 실수입니다.");	
 		System.out.println("3+5" + "의 연산결과는 8입니다.");
